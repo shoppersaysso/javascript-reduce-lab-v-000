2d0bc21fae91f7d399ea63ebe19707eefa6996a7
Sara Hopper <sara.hopper@gmail.com>
2017-08-29 23:41:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-  const totalBatteries = batteryBatches.reduce((total, amount) => total + amount);
+const totalBatteries = batteryBatches.reduce((total, amount) => total + amount);
 
 
 
